8c0bcdee3cf74b4476fff126192428216b559999
Guillermo Conde Flores <condeguillermo8@gmail.com>
2021-06-10 09:08:32 -0500

    Funcionalidad del botón

--- app/src/main/java/com/example/mensaje/MainActivity.java
+++ app/src/main/java/com/example/mensaje/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mensaje;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.boton);
+        TextView texto = (TextView) findViewById(R.id.mensaje);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (texto.getVisibility() == View.VISIBLE){
+                    texto.setVisibility(View.INVISIBLE);
+                } else {
+                    texto.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
+
+
 }
